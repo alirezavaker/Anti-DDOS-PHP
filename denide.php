90a1a2361733e36ef9fa91d5cd368503ad7c6a9f
attackeralireza <sarbazvatan74@gmail.com>
2021-09-23 16:10:58 +0330

    wirte code denide ui

---
+++ denide.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/style.css" />
+  </head>
+  <body>
+    
+    <!-- Loading -->
+    <h3 class="sub_title">IP : <span id="r_ip"><?php  $_SERVER['REMOTE_ADDR'];?></span></h3>
+    <p>لطفاً برای گذراندن مرحله حفاظتی بر روی لینک زیر را کلیک کنید</p>
+    <a href="#" id="link_pass"> link </a>
+    <p>
+      یا بعد از 5 ثانیه به صورت خودکار به صفحه درخواستی هدایت می شوید. برای
+      ادامه کار با وب سایت ، لطفاً مطمئن شوید که <span class="jsred">جاوا اسکریپت</span> را فعال کرده اید.
+    </p>
+    <p>
+        DDoS protection by CleanTalk
+welcome@cleantalk.org
+    </p>
+    <!-- Script -->
+    <script src="assets/denide.js"></script>
+  </body>
+</html>
