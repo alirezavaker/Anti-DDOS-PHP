811511a7e5b49849a13dda8d96ce92866f4a6480
attackeralireza <sarbazvatan74@gmail.com>
2021-09-23 16:05:37 +0330

    finally ddos code for now!

--- core/anti_ddos_main.php
+++ core/anti_ddos_main.php
@@ -35,3 +35,60 @@ function getRealIpAddr()
     }
     return $ip;
 }
+// هدایت کاربر
+function Redirect($permanent = false)
+{
+    $url = REDIRECT_URL_DENIDE;
+    if (headers_sent() === false) {
+        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+    }
+
+    exit();
+}
+
+if (file_exists(CONTROL_LOCK_FILE)) {
+    if (time() - filemtime(CONTROL_LOCK_FILE) > CONTROL_BAN_TIME) {
+        // این کاربر مجازات خود را کامل کرده است
+        unlink(CONTROL_LOCK_FILE);
+    } else {
+        // درخواست های زیاد
+        Redirect();
+        touch(CONTROL_LOCK_FILE);
+        die();
+    }
+}
+
+function antiflood_countaccess()
+{
+    //شمارش درخواست ها و آخرین زمان دسترسی
+    $control = array();
+
+    if (file_exists(CONTROL_DB)) {
+        $fh = fopen(CONTROL_DB, "r");
+        $control = array_merge($control, unserialize(fread($fh, filesize(CONTROL_DB))));
+        fclose($fh);
+    }
+
+    if (isset($control[USER_IP])) {
+        if (time() - $control[USER_IP]["t"] < CONTROL_REQ_TIMEOUT) {
+            $control[USER_IP]["c"]++;
+        } else {
+            $control[USER_IP]["c"] = 1;
+        }
+    } else {
+        $control[USER_IP]["c"] = 1;
+    }
+    $control[USER_IP]["t"] = time();
+
+    if ($control[USER_IP]["c"] >= CONTROL_MAX_REQUESTS) {
+        // این کاربر درخواستهای زیادی را در مدت زمان بسیار کوتاهی انجام داد
+        $fh = fopen(CONTROL_LOCK_FILE, "w");
+        fwrite($fh, USER_IP);
+        fclose($fh);
+    }
+    //نوشتن جدول کنترل به روز شده
+    $fh = fopen(CONTROL_DB, "w");
+    fwrite($fh, serialize($control));
+    fclose($fh);
+}
+antiflood_countaccess();
