d58201fd32ca37812d7774223e524901e801bb41
attackeralireza <sarbazvatan74@gmail.com>
2021-09-23 16:04:29 +0330

    add finder IP

--- core/anti_ddos_main.php
+++ core/anti_ddos_main.php
@@ -10,7 +10,7 @@ define("CONTROL_BAN_TIME", 4);
 // دایرکتوری قابل نوشتن برای نگهداری داده های اسکریپت
 define("SCRIPT_TMP_DIR", SCRIPT_ROOT . "/core");
 // !! نیازی به ویرایش در زیر این خط ندارید
-define("USER_IP", "Your IP");
+define("USER_IP", getRealIpAddr());
 define("CONTROL_DB", SCRIPT_TMP_DIR . "/log");
 define("CONTROL_LOCK_DIR", SCRIPT_TMP_DIR . "/lock");
 define("CONTROL_LOCK_FILE", CONTROL_LOCK_DIR . "/" . md5(USER_IP));
@@ -20,3 +20,18 @@ define('REDIRECT_URL_DENIDE', 'denide.php');
 // ساخت دایرکتوری
 @mkdir(CONTROL_LOCK_DIR);
 @mkdir(SCRIPT_TMP_DIR);
+
+function getRealIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) //بررسی IP از اشتراک اینترنت
+    {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    //to check ip is pass from proxy
+    {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
