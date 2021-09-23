ebefdef28783b18d2e8e5d107af5a4833373a81e
attackeralireza <sarbazvatan74@gmail.com>
2021-09-23 16:06:59 +0330

    Added index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php include_once 'main.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/style.css" />
+  </head>
+  <body>
+    <h1 class="title">
+     <span class="Raleway">
+          DDOS Protection
+     </span>
+      فعال است !
+    </h1>
+    <!-- Loading -->
+    <div id="loading" class="loadingio-spinner-ellipsis-h9tj8pt7aoa ltr">
+      <div class="ldio-a538nmc72j5">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+    <!-- Loading -->
+    <h3 class="sub_title">IP : <span id="r_ip"><?php  $_SERVER['REMOTE_ADDR'];?></span></h3>
+    <p>لطفاً برای گذراندن مرحله حفاظتی بر روی لینک زیر را کلیک کنید</p>
+    <a href="#" id="link_pass"> link </a>
+    <p>
+      یا بعد از 5 ثانیه به صورت خودکار به صفحه درخواستی هدایت می شوید. برای
+      ادامه کار با وب سایت ، لطفاً مطمئن شوید که <span class="jsred">جاوا اسکریپت</span> را فعال کرده اید.
+    </p>
+    <p>
+        DDoS protection by CleanTalk
+welcome@cleantalk.org
+    </p>
+    <!-- Script -->
+    <script src="assets/app.js"></script>
+  </body>
+</html>
